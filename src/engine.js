e1c2c17432aab39014d23866bc4f0abdf4e2154d
Steve Deng <dengzw1994@gmail.com>
2021-08-09 11:03:47 +1000

    upload changes

--- src/engine.js
+++ src/engine.js
@@ -1,8 +1,41 @@
 // (world: boolean[][]) => boolean[][]
 export const next = (world) => {
-  return world;
+  const rows = world.length;
+  const cols = world[0].length;
+
+  const nestedArray = Array.from({ length: rows }, () => 
+    Array.from({ length: cols }, () => false)
+  );
+  nestedArray.forEach((row, y) =>
+    row.forEach(
+      function(col, x) {
+        var number = 0;
+        var x_start = x >= 1 ? x - 1 : 0;
+        var y_start = y >= 1 ? y - 1 : 0;
+        var x_len = x == 0 || x == cols - 1 ? 1 : 2;
+        var y_len = y == 0 || y == rows - 1 ? 1 : 2;
+        for(var i = 0; i <= x_len; i++) {
+          for(var j = 0; j <= y_len; j++) {
+            if (i + x_start == x && j + y_start == y) continue;
+            if (world[j + y_start][i + x_start] == true) {
+              number++;
+            }
+          }
+        }
+
+        if (number == 2 || number == 3) {
+          row[x] = true;
+        } 
+    })
+  );
+  return nestedArray;
 };
 // (pattern: string) => boolean[][]
 export const parse = (pattern) => {
-  return [];
+  if (!pattern) return [];
+  var worldArr = pattern.split("\n").map(function(e) {
+    return e.split("").map(x => Boolean(x !== "."));
+  });
+  worldArr.splice(-1);
+  return worldArr;
 };
--- src/engine.test.js
+++ src/engine.test.js
@@ -17,7 +17,7 @@ describe("next", () => {
     ]);
   });
 
-  it.skip("should be dead if there is no neighbours", () => {
+  it("should be dead if there is no neighbours", () => {
     const result = next([
       [_, _, _],
       [_, o, _],
@@ -25,17 +25,78 @@ describe("next", () => {
     ]);
     expect(result[1][1]).to.equal(false);
   });
+
+  it("should be dead if there are four or more neighbours", () => {
+    const result = next([
+      [o, _, o],
+      [_, o, o],
+      [o, o, _],
+    ]);
+    expect(result[1][1]).to.equal(false);
+  });
+
+  it("should be dead if there are four or more neighbours #2", () => {
+    const result = next([
+      [o, _, o],
+      [o, o, o],
+      [_, o, _],
+    ]);
+    expect(result[1][1]).to.equal(false);
+    expect(result[2][0]).to.equal(true);
+  });
+
+  it("should survive if there are 2 or 3 neighbours", () => {
+    const result = next([
+      [_, _, o],
+      [_, o, o],
+      [o, _, _],
+    ]);
+    expect(result[1][1]).to.equal(true);
+    expect(result[1][2]).to.equal(true);
+  });
+
+  it("for empty cell, it should become populated if there are 3 neighbours", () => {
+    const result = next([
+      [_, _, o],
+      [_, _, o],
+      [o, _, _],
+    ]);
+    expect(result[1][1]).to.equal(true);
+    expect(result[0][2]).to.equal(false);
+  });
+
+  it("for empty cell, it should become populated if there are 3 neighbours", () => {
+    const result = next([
+      [_, _, o, _, _, o],
+      [_, o, o, _, _, _],
+      [o, _, _, _, o, _],
+      [_, _, o, _, _, o],
+      [_, _, _, _, _, _],
+      [o, _, _, _, _, _],
+    ]);
+    expect(result[5][5]).to.equal(false);
+    expect(result[2][1]).to.equal(false);
+    expect(result[2][2]).to.equal(true);
+  });
 });
 
 describe("parse", () => {
   it("should return [] given ''", () => {
     expect(parse("")).to.deep.equal([]);
   });
-  it.skip("should parse O as true and . as false", () => {
+  it("should parse O as true and . as false", () => {
     expect(parse("...\n.O.\n...\n")).to.deep.equal([
       [_, _, _],
       [_, o, _],
       [_, _, _],
+    ]);
+  });
+  it("should parse O as true and . as false", () => {
+    expect(parse("...0\n.00.\n..0.\n0...\n")).to.deep.equal([
+      [_, _, _, o],
+      [_, o, o, _],
+      [_, _, o, _],
+      [o, _, _, _],
     ]);
   });
 });
